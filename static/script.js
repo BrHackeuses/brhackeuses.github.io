20129b1a836a1d327d36275b28a0c998fdfedc72
Malek <malek.sfaxi@eurecom.fr>
2024-12-20 04:12:32 +0100

    update page

---
+++ static/script.js
@@ -0,0 +1,34 @@
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene);
+
+
+
+/*Hyperlinks*/
+const URL_DISCORD = "https://discord.gg/xqvnaGzG6x";
+
+
+var dicoUrl = {
+  discord: URL_DISCORD, 
+};
+
+const openNewTabWith = (site) => {
+  console.log("Opening " + site);
+  window.open(dicoUrl[site], "_blank");
+};
+
+
+
+
+//Cursor Background Tracking
+const tokenBgCursor = document.querySelector('.blob-cursor__bg');
+document.addEventListener('mousemove', e => {
+    tokenBgCursor.setAttribute("style", "top: " + (e.pageY - 100) + "px; left: " + (e.pageX - 100) + "px;")
+});
+const triggerCursor = document.querySelector('.show-cursor--bg');
+const cursorBg = document.querySelector('.blob-cursor__bg');
+triggerCursor.addEventListener('mouseenter', () => {
+    cursorBg.classList.remove('hide');
+});
+triggerCursor.addEventListener('mouseleave', () => {
+    cursorBg.classList.add('hide');
+});
